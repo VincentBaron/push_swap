33821094e3f577532f4bee6994211a1a1f6c54a2
VincentBaron <vbaron@student.42.fr>
2021-08-05 14:42:25 +0200

    less than 5 finished cleaned

--- srcs/less_than_five.c
+++ srcs/less_than_five.c
@@ -6,7 +6,7 @@
 /*   By: vincentbaron <vincentbaron@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 16:18:27 by vbaron            #+#    #+#             */
-/*   Updated: 2021/08/05 12:40:38 by vincentbaro      ###   ########.fr       */
+/*   Updated: 2021/08/05 14:41:08 by vincentbaro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,53 +25,35 @@ void calculate_offset(t_general *mother)
 
 void less_than_five(t_general *mother)
 {
-    int turn;
-    
-
-    if (is_sorted(&mother->stack_a))
-        return;
-    pb_operation(mother);
-    if (mother->stack_a.len == 4)
-        pb_operation(mother);
-    less_than_three(mother);
-    turn = 0;
-    while (!is_sorted(&mother->stack_a) || mother->stack_b.len)
+    while (!is_sorted(&mother->stack_a))
     {
-        calculate_offset(mother);
-        if (mother->offset == 0)
-            pa_operation(mother);
-        else if (mother->offset == mother->stack_a.len)
-        {
-            pa_operation(mother);
-            ra_operation(mother);
-        }
-        else if (mother->offset == 1)
-        {
-            pa_operation(mother);
-            sa_operation(mother);
-        }
-        else if (mother->offset == mother->stack_a.len - 1)
-        {
-            rra_operation(mother);
-            pa_operation(mother);
-        }
-        else if (mother->stack_b.len == 1 && mother->offset == 2)
-        {
-            ra_operation(mother);
-            ra_operation(mother);
-            pa_operation(mother);
-        }
-        if (mother->stack_b.len == 0 && !is_sorted(&mother->stack_a))
+        while (mother->stack_a.len > 3)
         {
             calculate_pos(&mother->stack_a);
-            if (mother->stack_a.max < (mother->stack_a.len < 2))
-                ra_operation(mother);
+            if (mother->stack_a.min <= mother->stack_a.len / 2)
+            {
+                while (mother->stack_a.min > 0)
+                {
+                    ra_operation(mother);
+                    mother->stack_a.min--;
+                }
+                
+            }
             else
-                rra_operation(mother);
-            
+            {
+                while (mother->stack_a.min <= mother->stack_a.len)
+                {
+                    rra_operation(mother);
+                    mother->stack_a.min++;
+                }
+            }
+            pb_operation(mother);
         }
-        turn++;
-        if (turn > 12)
+        less_than_three(mother);
+        pa_operation(mother);
+        pa_operation(mother);
+        mother->turn++;
+        if (mother->turn > 15)
             break;
     }
     ft_printf("\nturns: %d", mother->turn);
