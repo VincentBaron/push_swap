e2b00182697cab4877e17e2a3f9d0b6cb1d73b72
Vincent BARON <vbaron@student.42.fr>
2021-08-23 15:42:28 +0200

    is sorted initial added

--- srcs/program_main.c
+++ srcs/program_main.c
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 15:34:56 by vbaron            #+#    #+#             */
-/*   Updated: 2021/08/19 15:59:31 by vbaron           ###   ########.fr       */
+/*   Updated: 2021/08/23 15:41:22 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,8 @@ int	main(int ac, char **av)
 	init_vars(&mother);
 	mother.stack_length = ac - 1;
 	create_stacks(&mother, av);
+	if (is_sorted(&(mother.stack_a)))
+		return (0);
 	mother.turn = 0;
 	if (mother.stack_length <= 3)
 		less_than_three(&mother);
